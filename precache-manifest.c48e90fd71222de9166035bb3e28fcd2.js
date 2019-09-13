b1efffb93e7006afe8ca07136ce8f96ae2c192a8
alex-gurung <aag1234@gmail.com>
2019-09-13 01:49:21 -0400

    Updates

---
+++ precache-manifest.c48e90fd71222de9166035bb3e28fcd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08bab41b5ac7137aa1d0",
+    "url": "/static/js/main.08bab41b.chunk.js"
+  },
+  {
+    "revision": "591664a1c0dde4f94f41",
+    "url": "/static/js/1.591664a1.chunk.js"
+  },
+  {
+    "revision": "08bab41b5ac7137aa1d0",
+    "url": "/static/css/main.07df73d4.chunk.css"
+  },
+  {
+    "revision": "5c5a11436911503d70bbe40bdfbb838b",
+    "url": "/index.html"
+  }
+];
